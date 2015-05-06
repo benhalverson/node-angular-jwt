d81e5253753a9084e68844acf4984ed877db0b31
Ben Halverson <benhalverson33@gmail.com>
2015-05-06 02:13:28 -0700

    added body parser

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
 var app = express();
-
+var bodyParser = require('body-parser');
+var user = {
+  username: 'Ben',
+  password: 'BH'
+}
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/random-user', function(req, res){
   var user = faker.helpers.userCard();
@@ -12,6 +17,24 @@ app.get('/random-user', function(req, res){
   res.json(user);
 });
 
+app.post('/login', authenticate, function(req, res){
+
+});
+
 app.listen(3000, function(){
   console.log('App listening on localhost:3000');
 });
+
+
+//UTIL Functions
+
+function authenticate(req, res, next) {
+  var body = req.body;
+  if(!body.username || !body.password) {
+    res.status(400).end('Must provide username or password');
+  }
+  if (body.username !== user.username || body.password !== user.password) {
+    res.status(401).end('Username or password incorrect');
+  }
+  next();
+};
